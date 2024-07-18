1eb1d76b587964600a7065a8435ef56994455aa7
minyang3194 <50267545+minyang3194@users.noreply.github.com>
2024-07-18 23:40:20 +0800

    Update diy-part1.sh

--- X86/lede/diy-part1.sh
+++ X86/lede/diy-part1.sh
@@ -17,3 +17,8 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+# 添加istore
+echo >> feeds.conf.default
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+./scripts/feeds update istore
+./scripts/feeds install -d y -p istore luci-app-store
